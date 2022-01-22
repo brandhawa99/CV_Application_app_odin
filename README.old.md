e641da819f86d392777ac2ea4c94a9fd1bd8083b
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-01-22 14:47:32 -0800

    asdf

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# CV_Application_app_odin
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+
+class Header extends Component {
+
+    render(){
+        return(
+            <Header>
+                Create Your CV
+            </Header>
+        )
+    }
+}
+
+export default Header;
